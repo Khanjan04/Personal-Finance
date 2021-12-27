6cda348c9585bd70443e0865600b49709f5bf13c
Khanjan <khanjanvarma9@gmail.com>
2021-12-27 22:56:00 +0530

    Home Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <h2>Personal Finance</h2>
+        <div className="button">
+            <button type="button" class="btn btn-outline-primary">Income</button>
+        </div>
+        <div className="button">
+            <button type="button" class="btn btn-outline-primary">Expense</button>
+        </div>
+        <div className="button">
+            <button type="button" class="btn btn-outline-primary">Bank Account</button>
+        </div>
+        <div className="button">
+            <button type="button" class="btn btn-outline-primary">Investment</button>
+        </div>
+        <div className="button">
+            <button type="button" class="btn btn-outline-primary">Summary</button>
+        </div>
+      </div>
     </div>
   );
 }
